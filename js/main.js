448f776ae5f39a59e859438cb260ae3fc5ed2077
AlenaMau <alenamau3486@gmail.com>
2024-04-22 14:52:52 +0700

    4 и 5  задание

--- js/main.js
+++ js/main.js
@@ -8,8 +8,34 @@ let app = new Vue({
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inventory: 100,
         onSale: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral']
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        reduceToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
 
     }
+
 
 })
